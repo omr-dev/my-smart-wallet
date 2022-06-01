9b8597e3426312012806f45c4114e99f00b19996
omer <46997887+omr-dev@users.noreply.github.com>
2022-06-01 05:43:04 +0200

    Authentication (#1)
    * auth0 package in
    
    * auth0 connection established
    
    * welcome page authentication updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.scss";
-
+import { useAuth0 } from "@auth0/auth0-react";
+import { FaSpinner } from "react-icons/fa";
+import ProfileBar from "./components/ProfileBar";
 function App() {
-  return <div className="App"><h1>Welcome to My Smart Wallet</h1></div>;
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  return (
+    <div className="App">
+      <h1>My Smart Wallet</h1>
+      <ProfileBar />
+      {isLoading ? (
+        <div>
+          <FaSpinner className="spinner" />
+        </div>
+      ) : isAuthenticated ? (
+        <h2 className="loginMessageBox">{`Welcome, ${user.email} !`}</h2>
+      ) : (
+        <h2 className="loginMessageBox">Please log in to My Smart Wallet.</h2>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import App from "./App";
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain="dev-eikyrwwd.us.auth0.com" //TODO: fix as env variable
+    clientId="6u6nlonLcKY5f8Sy1tITBsxYTboyBoT7" //TODO: fix as env variable
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
-)
+  </Auth0Provider>
+);
